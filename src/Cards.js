5b5c144a4493b3397a89dc8605c889ee5229f0a5
vsel <vselyukh@gmail.com>
2017-08-27 17:07:45 +0300

    async await support added.

--- src/Cards.js
+++ src/Cards.js
@@ -4,8 +4,24 @@ import React from 'react';
  * A counter button: tap the button to increase the count.
  */
 export default class Cards extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        };
+    }
+
+    componentDidMount() {
+        async function getUserReposData() {
+            const response = await fetch('tailhook.json', {});
+            const json = await response.json();
+
+            return json;
+        }
+        getUserReposData().then((json) => { this.setState({ data: json }); });
+    }
+
     render() {
-        console.log(this.state);
         return (
             <div className='pure-g'>
                 <div className='pure-u-1 pure-u-md-1-4'>
