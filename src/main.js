e8faf3ea73c2e7a5c90173f0ba5cfb1dafda80ae
vsel <vselyukh@gmail.com>
2017-09-18 13:12:49 +0300

    router added.

--- src/main.js
+++ src/main.js
@@ -5,13 +5,16 @@ import { createStore } from 'redux';
 import CardsContainer from './prereactrouter';
 import { todos } from './reducers';
 // import SearchDialog from './Search';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const store = createStore(todos, { data: [] });
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
         <Provider store={store}>
-            <CardsContainer />
+            <BrowserRouter>
+                <Route path='/' component={CardsContainer} />
+            </BrowserRouter>
         </Provider>,
         document.getElementById('mount')
     );
